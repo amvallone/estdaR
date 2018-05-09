2acbb33871ae36cfd216f9809508586e4ff1ae8d
Andres Vallone <avallone@ucn.cl>
2018-05-09 13:08:48 +0200

    Fisrt installable version

---
+++ R/mexico.R
@@ -0,0 +1,8 @@
+#' Mexican states regional income 1940-2000
+#'
+#' Data from Mexican states regional income from 1940-2000 used in Rey and Guiterrez (2010)
+#' @docType data
+#' #usage data(mexico)
+#' @format A data frame contaning...
+#' @references Rey, S.J. and M.L. Sastre Gutierrez. (2010) "Interregional inequality dynamics in Mexico." Spatial Economic Analysis, 5: 277-298.
+#' @source \href{https://github.com/pysal/pysal/tree/master/pysal/examples/mexico}
